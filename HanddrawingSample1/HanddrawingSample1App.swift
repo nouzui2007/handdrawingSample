35bbbde6d85de3d00664ab022c3ea63d9ad4b3f6
Hotaka Makiuchi <makiuchi@ccube.jp>
2023-09-17 10:03:01 +0900

    Initial Commit

---
+++ HanddrawingSample1/HanddrawingSample1App.swift
@@ -0,0 +1,17 @@
+//
+//  HanddrawingSample1App.swift
+//  HanddrawingSample1
+//
+//  Created by 牧内 on 2023/09/17.
+//
+
+import SwiftUI
+
+@main
+struct HanddrawingSample1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HanddrawingSample1UITests/HanddrawingSample1UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  HanddrawingSample1UITestsLaunchTests.swift
+//  HanddrawingSample1UITests
+//
+//  Created by 牧内 on 2023/09/17.
+//
+
+import XCTest
+
+final class HanddrawingSample1UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
